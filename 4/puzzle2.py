4db20a79eadb8a43f23c5cde79bdc890555e678e
Curtis Maddalozzo <curtis@tractable.ai>
2018-12-04 22:17:26 +0000

    Day 4 🎄

---
+++ 4/puzzle2.py
@@ -0,0 +1,33 @@
+import re
+
+naps = {}
+
+logs = [log.strip() for log in open('./input.txt')]
+logs.sort()
+
+
+re_shift_start = re.compile('.+Guard #(?P<id>\\d+) begins shift$')
+re_time = re.compile('\[[^:]+:(?P<time>\\d{2})\]')
+
+i = 0
+curr_guard_id = None
+while i < len(logs):
+    m = re_shift_start.match(logs[i])
+    if m:
+        curr_guard_id = m.group('id')
+        if curr_guard_id not in naps:
+            naps[curr_guard_id] = [0] * 60
+        i += 1
+    else:
+        start = int(re_time.match(logs[i]).group('time'))
+        end = int(re_time.match(logs[i+1]).group('time'))
+        for j in range(start, end):
+            naps[curr_guard_id][j] += 1
+        i += 2
+
+sleepiest = sorted(naps.keys(), key=lambda g: max(naps[g]))[-1]
+
+sleepiest_minute = max(naps[sleepiest])
+
+print(sleepiest)
+print(naps[sleepiest].index(sleepiest_minute))
